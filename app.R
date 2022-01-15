2964ae26805e722281201c4b3e3ac65b17af763b
Abulily <4079204+vschaftenaar@users.noreply.github.com>
2022-01-15 10:28:41 +0100

    Add files via upload

---
+++ app.R
@@ -0,0 +1,5 @@
+if(!require(abulily))devtools::install_github('vschaftenaar/abulily',force = T)
+
+print(abulily::get_directory())
+
+Sys.sleep(5)
---
+++ run.bat
@@ -0,0 +1,14 @@
+@echo off&setlocal
+title title of the application
+
+REM Search for R-script
+for /d /r "C:\Program Files\R\"  %%a in ("R-*") do set "folderpath=%%a\bin\Rscript.exe"
+
+
+REM Name of the app to run
+set appname=app.R
+
+REM Run app
+"%folderpath%" "%~dp0%appname%"
+
+pause
